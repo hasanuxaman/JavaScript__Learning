8bff993d5d64646f646556ea5e8ab663af16585b
Md Hasanuzzaman Rony <hasanuxaman.rony@hotmail.com>
2025-11-17 15:47:25 +0600

    first commit

---
+++ JavaScript.js
@@ -0,0 +1,23 @@
+// const numbers = [1, 2, 3, 4, 5]
+// console.log(Array.isArray(numbers))
+// const number = 100
+// console.log(Array.isArray(number)) // false
+// const numberss = [1, 2, 3, 4, 5]
+// console.log(numbers.toString()) // 1,2,3,4,5
+// const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+// console.log(names.toString()) // Asabeneh,Mathias,Elias,Brook
+// const numberssss = [1, 2, 3, 4, 5]
+// console.log(numbers.join()) // 1,2,3,4,5
+
+
+
+// const numbers = [1, 2, 3, 4, 5]
+// //console.log(numbers.slice()) // -> it copies all  item
+// console.log(numbers.slice(1,3)) // -> it copies all  item
+// //onsole.log(numbers.slice(0, numbers.length)) // it copies all  item
+// // console.log(numbers.slice(1, 4)) // -> [2,3,4] // it doesn't include the ending position
+
+
+const numbers = [1, 2, 3, 4, 5]
+
+console.log(numbers.splice(2)) 
